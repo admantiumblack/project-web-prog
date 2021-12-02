5892ba757e0dbdef19bf50483e66c91007148fd5
snosw <54971768+snosw@users.noreply.github.com>
2021-12-02 09:19:43 +0700

    Initial custom formatting and layout

---
+++ project-web-prog/resources/views/home.blade.php
@@ -0,0 +1,33 @@
+@extends('layout.master')
+
+@section('title', 'Placeholder title')
+
+@section('content')
+
+    <div class="c-page-region">
+        <h3>Home</h3>
+    </div>
+    <hr class="my-0" />
+    <div class="c-page-region">
+        <div class="d-flex flex-row">
+            <div class="col">
+                <div class="mb-1">Section title 1</div>
+                <div class="card">
+                    <div class="c-card-content">
+                        Section content
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="mb-1">Section title 2</div>
+                <div class="card">
+                    <div class="c-card-content">
+                        Section content
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
