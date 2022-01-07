d1e86a44e45d5e9e79336460459db748a057ca3d
Roland <62351628+admantiumblack@users.noreply.github.com>
2022-01-07 11:56:59 +0700

    added login and register route

--- project-web-prog/routes/api.php
+++ project-web-prog/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthenticationAPIController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/auth/login', [AuthenticationAPIController::class, 'login'])->name('api.login');
+Route::get('/auth/logout', [AuthenticationAPIController::class, 'logout'])->name('api.logout');
