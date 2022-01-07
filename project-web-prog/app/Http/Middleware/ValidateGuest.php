a1a768dbe29e939b6f32608960720092d543c324
Roland <62351628+admantiumblack@users.noreply.github.com>
2022-01-07 16:25:44 +0700

    Create ValidateGuest.php

---
+++ project-web-prog/app/Http/Middleware/ValidateGuest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ValidateGuest
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if($request->hasCookie('user_auth')){
+            return redirect()->back();
+        }
+        return $next($request);
+    }
+}
