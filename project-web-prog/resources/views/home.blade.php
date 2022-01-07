bb1fe80c1c12b93674c9fa966058b8184d354529
snosw <54971768+snosw@users.noreply.github.com>
2022-01-07 08:37:59 +0700

    Updated home components to Bootstrap 5.1

--- project-web-prog/resources/views/home.blade.php
+++ project-web-prog/resources/views/home.blade.php
@@ -4,11 +4,11 @@
 
 @section('content')
 
-    <div class="d-flex flex-row">
-        <div class="col col-9">
+    <div class="row row-cols-1">
+        <div class="col">
             <div class="mb-1">Forms</div>
             <div class="card">
-                <div class="c-card-content">
+                <div class="card-body">
                     <div class="list-group">
                         @forelse($forms as $form)
                             <a class="list-group-item list-group-item-action" href="/form">
@@ -25,22 +25,22 @@
         <div class="col">
             <div class="mb-1">Complaint Ticket</div>
             <div class="card">
-                <button type="button" class="c-card-content btn btn-danger text-white" id="ticket-btn" data-toggle="modal" data-target="#exampleModal">
+                <button type="button" class="card-body btn btn-danger text-white" id="ticket-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     New complaint ticket
                 </button>
                 <div class="modal fade" id="exampleModal" tabindex="-1">
                     <div class="modal-dialog">
                         <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                        </div>
-                        <div class="modal-body">
-                            {{-- TO-DO Complaint ticket form --}}
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                        </div>
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            </div>
+                            <div class="modal-body">
+                                SAMPLE TEXT FOR BODY
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
                         </div>
                     </div>
                     </div>
