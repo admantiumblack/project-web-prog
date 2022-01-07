bc1c1439024f40e31ada4b4ca13913569f696adb
Roland <62351628+admantiumblack@users.noreply.github.com>
2022-01-07 09:49:01 +0700

    made api controller

---
+++ project-web-prog/app/Http/Controllers/AuthenticationAPIController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthenticationAPIController extends Controller
+{
+    public function login(Request $request){
+
+    }
+}
