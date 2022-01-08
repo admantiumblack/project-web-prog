4cd23bcc54e72d243512a3438f633da158752654
Roland <62351628+admantiumblack@users.noreply.github.com>
2022-01-08 11:27:49 +0700

    home update

--- project-web-prog/app/Http/Controllers/HomeController.php
+++ project-web-prog/app/Http/Controllers/HomeController.php
@@ -14,10 +14,17 @@ class HomeController extends Controller
         $forms = DB::table('forms')
         ->join('subject_lecturers', 'forms.subject_id', '=', 'subject_lecturers.subject_id')
         ->join('subjects', 'forms.subject_id', '=', 'subjects.id')
-        ->select('forms.*', 'subjects.subject', 'subject_lecturers.lecturer_id')
+        ->select('forms.*', 'subjects.subject', 'subject_lecturers.lecturer_id', 'subject_lecturers.period as lecture_period')
         ->where('subject_lecturers.lecturer_id', '=', $id)
-        ->where('forms.period', '=', 'subject_lecturers.period')
+        ->where('forms.deadline', '>', date('Y-m-d H:i:s', time()))
         ->get();
+
+        $newForms = [];
+        foreach($forms as $form){
+            if($form->lecture_period == $form->period){
+                $newForms[] = $form;
+            }
+        }
 
         $subjectLecturers = SubjectLecturer::where('lecturer_id', $id)
                             ->with('subject')->get();
