aeffe64c6aaf99326f83c28b94f1d54808427a46
Roland <62351628+admantiumblack@users.noreply.github.com>
2022-01-09 16:05:16 +0700

    Update SCCController.php

--- project-web-prog/app/Http/Controllers/SCCController.php
+++ project-web-prog/app/Http/Controllers/SCCController.php
@@ -16,11 +16,9 @@ class SCCController extends Controller
         $cluster_id = ClusterScc::where('lecturer_id','=',$ssc_id)->first()->cluster_id;
 
         $forms = DB::table('forms')
-        ->join('subject_lecturers', 'forms.subject_id', '=', 'subject_lecturers.subject_id')
         ->join('subjects', 'forms.subject_id', '=', 'subjects.id')
-        ->select('forms.*', 'subjects.subject', 'subject_lecturers.lecturer_id', 'subject_lecturers.period as lecture_period', 'subject_lecturers.has_Filled_form as has_filled_form')
+        ->select('forms.*', 'subjects.subject')
         ->where('subjects.cluster_id', '=', $cluster_id)
-        ->whereRaw('forms.period = subject_lecturers.period')
         ->orderBy('forms.period', 'Desc')
         ->get();
         
