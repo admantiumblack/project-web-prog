f2a7f9fe5abc3e5b23343d8dca4affb4ee002e61
Roland <62351628+admantiumblack@users.noreply.github.com>
2022-01-09 20:36:55 +0700

    Update LecturerSeeder.php

--- project-web-prog/database/seeders/LecturerSeeder.php
+++ project-web-prog/database/seeders/LecturerSeeder.php
@@ -19,7 +19,7 @@ class LecturerSeeder extends Seeder
         $faker->seed(666);
         $myfile = fopen("../.passwords", "w");
         for($i = 0; $i < 60; $i++){
-            $password = $faker->password(10, 20);
+            $password = 'password';
             $name = $faker->unique()->name;
             $lecturerId = 'D'.$faker->unique()->regexify('[0-5]{1}[0-9]{3}');
             $email = 'blackadam123455+'.$lecturerId.'_'.explode(' ',trim($name))[0].'@gmail.com';
@@ -28,7 +28,7 @@ class LecturerSeeder extends Seeder
             DB::table('lecturers')->insert([
                 'id' => $lecturerId,
                 'name' => $name,
-                'password' => Hash::make('password'),
+                'password' => Hash::make($password),
                 'phone_number' => $faker->phoneNumber,
                 'email' => $email,
                 'position_id' => $position
