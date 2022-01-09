fc9f6b01ad1709c7904c64750934ffa6d8129dd9
Ariel Peaceo Gunawan <93077469+Kouci01@users.noreply.github.com>
2022-01-09 19:44:55 +0700

    Finished Manage Subject

--- project-web-prog/app/Http/Controllers/SubjectController.php
+++ project-web-prog/app/Http/Controllers/SubjectController.php
@@ -2,8 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cluster;
+use App\Models\Lecturer;
+use App\Models\SubjectLecturer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use PhpParser\Node\Expr\FuncCall;
 
 class SubjectController extends Controller
-{
+{   
+    public function ManageSubject(){
+        
+        $clusters = Cluster::get();
+        $periods = SubjectLecturer::distinct()->pluck('period');
+        
+        $clusterfirst = Cluster::get()->first()->id;
+        $periodfirst = SubjectLecturer::distinct()->pluck('period')->first();
+
+        $dosens = DB::table('lecturers')
+        ->join('subject_lecturers','subject_lecturers.lecturer_id','=','lecturers.id')
+        ->join('subjects','subjects.id','=','subject_lecturers.subject_id')
+        ->where('subject_lecturers.period', '=',$periodfirst)
+        ->where('subjects.cluster_id','=',$clusterfirst)
+        ->select('lecturers.*','subjects.subject')
+        ->get();
+
+        return view('manage.subjects', ['clusters' => $clusters, 'periods'=>$periods, 'dosens'=>$dosens, 'cluster'=>$clusterfirst, 'period' =>$periodfirst]);
+    }
+
+    public function ManageSubjectbyClusandPe(Request $request){
+        $request->validate([
+            'cluster' => 'required',
+            'period' => 'required'
+        ]);
+
+        $clusters = Cluster::get();
+        $periods = SubjectLecturer::distinct()->pluck('period');
+        
+        $cluster = $request->cluster;
+        $period = $request->period;
+
+        $dosens = DB::table('lecturers')
+        ->join('subject_lecturers','subject_lecturers.lecturer_id','=','lecturers.id')
+        ->join('subjects','subjects.id','=','subject_lecturers.subject_id')
+        ->where('subject_lecturers.period', '=',$period)
+        ->where('subjects.cluster_id','=',$cluster)
+        ->select('lecturers.*','subjects.subject')
+        ->get();
+
+        return view('manage.subjects', ['clusters' => $clusters, 'periods'=>$periods, 'dosens'=>$dosens, 'cluster'=>$cluster, 'period' =>$period]);
+    }
 }
--- project-web-prog/routes/web.php
+++ project-web-prog/routes/web.php
@@ -5,7 +5,9 @@ use App\Http\Controllers\ComplaintController;
 use App\Http\Controllers\FormController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SCCController;
+use App\Http\Controllers\SubjectController;
 use Illuminate\Support\Facades\Route;
+use Monolog\Handler\RotatingFileHandler;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,6 +33,10 @@ Route::get('/forms', [FormController::class, 'viewAllForm'])->name('view.forms')
 Route::get('/feedbacks', [ComplaintController::class, 'viewAllFeedback'])->name('view.complaints')->middleware('validateSCC');
 
 Route::get('/form/{id}', [FormController::class, 'viewInputForm'])->name('form')->middleware('validateLoggedIn');
+
+Route::get('/manage', [SubjectController::class, 'ManageSubject'])->name('manage.subjects')->middleware('validateDean');
+
+Route::post('/manage', [SubjectController::class, 'ManageSubjectbyClusandPe']);
 
 Route::get('/forms/{formId}', 
     [FormController::class, 'viewFormDetail'])
