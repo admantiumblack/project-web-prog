a88a65218b54f31c8746e4c3fd360cbfe4872af5
snosw <54971768+snosw@users.noreply.github.com>
2022-01-09 23:26:02 +0700

    Edited placement in view.formdetail + removed test charts in formController

--- project-web-prog/app/Http/Controllers/FormController.php
+++ project-web-prog/app/Http/Controllers/FormController.php
@@ -198,48 +198,6 @@ class FormController extends Controller
                 }
             }
         }
-
-
-        
-        $chartjstest1 = app()->chartjs
-             ->name('barChartTest')
-             ->type('pie')
-            //  ->size(['width' => 400, 'height' => 200])
-             ->labels(['Label x', 'Label y'])
-             ->datasets([
-                 [
-                     "label" => "My First dataset",
-                     'backgroundColor' => ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
-                     'data' => [69, 59]
-                 ],
-                 [
-                     "label" => "My First dataset",
-                     'backgroundColor' => ['rgba(255, 99, 132, 0.3)', 'rgba(54, 162, 235, 0.3)'],
-                     'data' => [65, 12]
-                 ]
-             ])
-             ->options(['maintainAspectRatio' => false]);
-
-        $chartjstest2 = app()->chartjs
-             ->name('barChartTest2')
-             ->type('bar')
-            //  ->size(['width' => 400, 'height' => 200])
-             ->labels(['Edited label', 'Edited label2'])
-             ->datasets([
-                 [
-                     "label" => "My First dataset",
-                     'backgroundColor' => ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
-                     'data' => [69, 59]
-                 ],
-                 [
-                     "label" => "My First dataset",
-                     'backgroundColor' => ['rgba(255, 99, 132, 0.3)', 'rgba(54, 162, 235, 0.3)'],
-                     'data' => [65, 12]
-                 ]
-             ])
-             ->options(['maintainAspectRatio' => false]);
-
-        $charttest = [$chartjstest1, $chartjstest2];
                     
         return view('view.formdetail', compact('answers', 'forms'))
             ->with('filled', $hasFilled)
