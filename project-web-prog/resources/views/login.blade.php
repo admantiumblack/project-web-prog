917312b14f3fdc45049f7ae90c2599299544eaaa
SeemssPro <Ssammyang@gmail.com>
2022-01-09 20:38:31 +0700

    Add 404 Page, and Update Login

--- project-web-prog/resources/views/login.blade.php
+++ project-web-prog/resources/views/login.blade.php
@@ -1,8 +1,18 @@
-@extends('layout.master')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('title', 'App Peer Review')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>App Peer Review</title>
+    <link rel="icon" type="image/png" href="https://bm5cdn.azureedge.net/newdefault/images/favicon.ico">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+</head>
 
-@section('content')
+<body>
     <div class="position-absolute card top-50 start-50 translate-middle c-card-350">
         <div class="card-body p-4">
             <img src="https://bm5cdn.azureedge.net/asset/images/logo.png" alt="Binus University">
@@ -14,9 +24,9 @@
                 @csrf
                 <div class="input-group">
                     <span class="input-group-text c-addon-42"><i class="far fa-envelope"></i></span>
-                    <input type="text" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" name="email"
-                        placeholder="Email" value="{{ old('email') }}" aria-label="Email" required autofocus
-                        autocomplete="email">
+                    <input type="text" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}"
+                        name="email" placeholder="Email" value="{{ old('email') }}" aria-label="Email" required
+                        autofocus autocomplete="email">
                 </div>
                 <div class="input-group mt-3">
                     <span class="input-group-text c-addon-42"><i class="fas fa-lock"></i></span>
@@ -35,5 +45,9 @@
 
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</body>
 
-@endsection
+</html>
