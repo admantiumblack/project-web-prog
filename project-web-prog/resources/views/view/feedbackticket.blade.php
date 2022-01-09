ded11ef651567c0c93a2930e8291582813e25f84
SeemssPro <Ssammyang@gmail.com>
2022-01-09 20:02:38 +0700

    Update View Feedback final And formresult

--- project-web-prog/resources/views/view/feedbackticket.blade.php
+++ project-web-prog/resources/views/view/feedbackticket.blade.php
@@ -3,45 +3,47 @@
 @section('title', 'Feedback Ticket')
 
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            <h1 class="text-center">Feedback Ticket</h1>
+    <div class="container-xxl mt-4">
+        <div class="px-2 mb-3 text-center">
+            <h1>Feedback Ticket</h1>
         </div>
-        <div class="card-body">
-            <table class="table table-light align-middle">
-                <thead>
-                    <tr>
-                        <th scope="col">No</th>
-                        <th scope="col">Course Id</th>
-                        <th scope="col">Course Name</th>
-                        <th scope="col">Issue</th>
-                        <th scope="col">Content</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse ($complaints as $key => $complaint)
+        <div class="card">
+            <div class="card-body">
+                <table class="table border border-3 border-dark align-middle">
+                    <thead class="table-dark">
                         <tr>
-                            <th>
-                                {{ $key+1 }}
-                            </th>
-                            <th>
-                                {{ $complaint->subject_id }}
-                            </th>
-                            <th>
-                                {{ $complaint->subject }}
-                            </th>
-                            <th>
-                                {{ $complaint->title }}
-                            </th>
-                            <th>
-                                {{ $complaint->content }}
-                            </th>
+                            <th class="col-1 text-center">No</th>
+                            <th class="col-1 text-center">Course Id</th>
+                            <th class="col-1 text-center">Course Name</th>
+                            <th class="col-2 text-center">Issue</th>
+                            <th class="col-5 text-center">Content</th>
                         </tr>
-                    @empty
-                        <h2 class="text-center">No Feedback Recently</h2>
-                    @endforelse
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @forelse ($complaints as $key => $complaint)
+                            <tr>
+                                <td class="text-center">
+                                    {{ $key + 1 }}
+                                </td>
+                                <td class="text-center">
+                                    {{ $complaint->subject_id }}
+                                </td>
+                                <td class="">
+                                    {{ $complaint->subject }}
+                                </td>
+                                <td class="">
+                                    {{ $complaint->title }}
+                                </td>
+                                <td class="">
+                                    {{ $complaint->content }}
+                                </td>
+                            </tr>
+                        @empty
+                            <h2 class="text-center">No Feedback Recently</h2>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 @endsection
--- project-web-prog/resources/views/view/formresult.blade.php
+++ project-web-prog/resources/views/view/formresult.blade.php
@@ -4,37 +4,37 @@
 
 @section('content')
 
-    <div class="row row-cols-1 row-cols-lg-2 p-3 m-2">
-        <div class="col-lg-9">
-            {{-- <div class="px-2 mb-3"><h1>All Forms - {{ rumpun }}</h1></div> --}}
-            <div class="px-2 mb-3"><h1>All Forms - Microsystems</h1></div>
-            <div class="card">
-                <div class="card-body">
-                    <table class="table align-middle">
-                        <thead>
+    <div class="container-xxl mt-4">
+        {{-- <div class="px-2 mb-3"><h1>All Forms - {{ rumpun }}</h1></div> --}}
+        <div class="px-2 mb-3 text-center">
+            <h1>All Forms - Microsystems</h1>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <table class="table border border-3 border-dark align-middle">
+                    <thead class="table-dark">
+                        <tr>
+                            <th class="col-5 text-center">Mata kuliah</th>
+                            <th class="col-1 text-center">Period</th>
+                            <th class="col-3 text-center">Deadline</th>
+                            <th class="col-2 text-center">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($forms as $key => $form)
                             <tr>
-                                <th class="col-5">Mata kuliah</th>
-                                <th class="col-2">Period</th>
-                                <th class="col-3">Deadline</th>
-                                <th class="col-2">Action</th>
+                                <td>{{ $form->subject }}</td>
+                                <td class="text-center">{{ $form->period }}</td>
+                                <td class="text-center">{{ $form->deadline }}</td>
+                                <td class="text-center">
+                                    <a class="btn btn-primary" href="#">View Details</a>
+                                </td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            @forelse($forms as $form)
-                                <tr>
-                                    <td>{{$form->subject}}</td>
-                                    <td>{{$form->period}}</td>
-                                    <td>{{$form->deadline}}</td>
-                                    <td>
-                                        <a class="btn btn-primary" href="#">View</a>
-                                    </td>
-                                </tr>
-                            @empty
-                            @endforelse
-                        </tbody>
-                        
-                    </table>
-                </div>
+                        @empty
+                        @endforelse
+                    </tbody>
+
+                </table>
             </div>
         </div>
     </div>
