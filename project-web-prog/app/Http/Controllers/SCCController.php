f402a4426b78c84b7089f14cb532539313018cc5
Ariel Peaceo Gunawan <93077469+Kouci01@users.noreply.github.com>
2022-01-09 11:23:59 +0700

    Update Complaint Function

--- project-web-prog/app/Http/Controllers/SCCController.php
+++ project-web-prog/app/Http/Controllers/SCCController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ClusterScc;
+use App\Models\Complaint;
 use Illuminate\Http\Request;
 
 class SCCController extends Controller
@@ -11,7 +13,16 @@ class SCCController extends Controller
         return view('view.formresult');
     }
 
-    public function viewAllFeedback(){
-        return view('view.feedbackticket');
+    public function viewAllFeedback(Request $request){
+
+        $ssc_id = explode('_', $request->cookie('user_auth'))[0];
+        $cluster_id = ClusterScc::where('lecturer_id','=',$ssc_id)->first()->cluster_id;
+        $complaints = Complaint::join('subjects', 'subjects.id', '=', 'complaints.subject_id')->where('subjects.cluster_id','=',$cluster_id)->get();
+        return view('view.feedbackticket', ['complaints'=>$complaints]);
     }
+
+    public function viewFeedbackbyPeriod(){
+
+    }
+
 }
