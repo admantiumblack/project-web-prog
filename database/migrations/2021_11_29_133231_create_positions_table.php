a399eb67abe0f832427f30ed2245abfc39d5b327
snosw <54971768+snosw@users.noreply.github.com>
2022-01-11 00:44:46 +0700

    Update 2021_11_29_133231_create_positions_table.php
    Fix attempt Roland edition

--- database/migrations/2021_11_29_133231_create_positions_table.php
+++ database/migrations/2021_11_29_133231_create_positions_table.php
@@ -14,7 +14,7 @@ class CreatePositionsTable extends Migration
     public function up()
     {
         Schema::create('positions', function (Blueprint $table) {
-            $table->id();
+            $table->increments("id");
             $table->string('position', 255);
         });
     }
